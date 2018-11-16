9ee0e4a634745be15c18ea0a7ce2080d3dafd963
yanghao <1785230164>
2018-11-16 11:26:03 +0800

    升级

---
+++ app/src/main/java/com/goose/app/model/App.java
@@ -0,0 +1,21 @@
+package com.goose.app.model;
+
+/**
+ * Created by taoyr on 2018/7/13.
+ */
+
+public class App {
+    public Integer id;
+
+    public Integer versionCode;
+
+    public String versionName;
+
+    public String address;
+
+    public String must;
+
+    public String type;
+
+    public String mark;
+}
